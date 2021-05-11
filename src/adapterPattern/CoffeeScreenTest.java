8f8fbe8b0745670652c3a04d602d6873138fb78f
Bruce <bruce.baranowski@gmail.com>
2021-05-11 00:41:18 -0400

    Added tester

---
+++ src/adapterPattern/CoffeeScreenTest.java
@@ -0,0 +1,14 @@
+package adapterPattern;
+
+public class CoffeeScreenTest {
+
+	public static void main(String[] args) {
+		CoffeeTouchscreenAdapter touchscreen = new CoffeeTouchscreenAdapter();
+
+		touchscreen.chooseFirstSelection();
+		
+		touchscreen.chooseSecondSelection();
+
+	}
+
+}
--- src/adapterPattern/OldCoffeeMachine.java
+++ src/adapterPattern/OldCoffeeMachine.java
@@ -3,10 +3,10 @@ package adapterPattern;
 public class OldCoffeeMachine {
 
 	public void selectA() {
-		
+		System.out.println("One cup of Coffee A");
 	}
 	
 	public void selectB() {
-		
+		System.out.println("One cup of Coffee B");
 	}
 }
